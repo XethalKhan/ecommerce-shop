1870c8620819e25c90d4e91175e982f3adf3dcbf
BZVZ <BZVZ@gmail.com>
2019-05-16 14:57:43 +0200

    index.php modified for routing

--- index.php
+++ index.php
@@ -28,78 +28,25 @@
 </head>
 <body class="container-fluid">
 	<?php 
-			error_reporting(E_ALL);
+		error_reporting(E_ALL);
 
-			require_once("views/fixed/header.php");
-			require_once("views/fixed/login.php");
-			require_once("views/fixed/nav.php");
+		require_once("views/fixed/header.php");
+		require_once("views/fixed/login.php");
+		require_once("views/fixed/nav.php");
 
-			require_once("config/connection.php");
-	?>
-	<div id="content" class="row">
-		<div id="main" class="col-sm-9">
-			<?php 
-					$stmt = $conn->prepare("SELECT * FROM product");
-					$stmt->execute();
-					$rs=$stmt->fetchall();
-					foreach($rs as $prod){
-					echo "<div class=\"row product\">" .
-							"<div class=\"col-sm-4 text-center product-img\">" .
-								"<img src=\"assets/images/" . $prod->img . "\" width=\"auto\" height=\"200\"/>" .
-							"</div>" .
-							"<div class=\"col-sm-8\">" .
-								"<div class=\"row text-center\">" .
-									"<div class=\"col-sm-12 product-name\">" .
-										"<h5>" . $prod->name . "</h5>" .
-									"</div>" .		
-								"</div>" .
-								"<div class=\"row\">" .
-									"<div class=\"col-sm-12\">" .
-										"<p class=\"product-desc\">" . $prod->short_desc . "</p>" .
-									"</div>" .		
-								"</div>" .
-								"<div class=\"row text-center\">" .
-									"<div class=\"col-sm-12\">" .
-										"<h5 class=\"product-price\">" . 
-											"$".number_format((float)$prod->unit_price, 2, ",", ".") . 
-										"</h5>" .
-									"</div>" .
-								"</div>" .
-								"<div class=\"row text-center product-links\">" .
-									"<div class=\"col-sm-6\">" .
-										"<a href=\"prod.php?pid=" . $prod->id . "\" class=\"product-detail\">Details</a>" .
-									"</div>" .
-									"<div class=\"col-sm-6\">" .
-										"<a href=\"\" class=\"product-action\" data-pid=\"" . $prod->id . "\" 
-									data-cat=\"" . $prod->cat_id . "\">Purchase</a>" .
-									"</div>" .		
-								"</div>" .
-							"</div>" .
-						"</div>";
-					}
-				?>
-		</div>
-		<div id="aside" class="col-sm-3">
-			<div class="row text-center">
-				<h3 class="col-sm-12">Categories</h3>
-			</div>
-			<div class="row text-center">
-				<ul class="col-sm-12">
-				<?php 
-					$stmt = $conn->prepare("SELECT * FROM category");
-					$stmt->execute();
-					$rs=$stmt->fetchall();
-					foreach($rs as $cat){
-						$n = strlen($cat->name) > 25 ? substr($cat->name, 0, 25) . ". . ." : $cat->name;
-						echo "<li><a href=\"products.php?cat=" . $cat->id . "\">" . $n ."</a></li>";
-					}
-				?>
-				</ul>
-			</div>
-		</div>
-	</div>
-	<?php
-			require_once("views/fixed/footer.php");
+		require_once("config/connection.php");
+
+		$page = isset($_GET["page"]) ? (empty($_GET["page"]) ? "home" : $_GET["page"]) : "home";
+		switch($page){
+			case "home":
+				require_once("views/pages/home.php");
+				break;
+			default:
+				require_once("views/pages/home.php");
+				break;
+		}
+
+		require_once("views/fixed/footer.php");
 	?>
 </body>
 </html>
---
+++ views/pages/home.php
@@ -0,0 +1,62 @@
+<div id="content" class="row">
+		<div id="main" class="col-sm-9">
+			<?php 
+					$stmt = $conn->prepare("SELECT * FROM product");
+					$stmt->execute();
+					$rs=$stmt->fetchall();
+					foreach($rs as $prod){
+					echo "<div class=\"row product\">" .
+							"<div class=\"col-sm-4 text-center product-img\">" .
+								"<img src=\"assets/images/" . $prod->img . "\" width=\"auto\" height=\"200\"/>" .
+							"</div>" .
+							"<div class=\"col-sm-8\">" .
+								"<div class=\"row text-center\">" .
+									"<div class=\"col-sm-12 product-name\">" .
+										"<h5>" . $prod->name . "</h5>" .
+									"</div>" .		
+								"</div>" .
+								"<div class=\"row\">" .
+									"<div class=\"col-sm-12\">" .
+										"<p class=\"product-desc\">" . $prod->short_desc . "</p>" .
+									"</div>" .		
+								"</div>" .
+								"<div class=\"row text-center\">" .
+									"<div class=\"col-sm-12\">" .
+										"<h5 class=\"product-price\">" . 
+											"$".number_format((float)$prod->unit_price, 2, ",", ".") . 
+										"</h5>" .
+									"</div>" .
+								"</div>" .
+								"<div class=\"row text-center product-links\">" .
+									"<div class=\"col-sm-6\">" .
+										"<a href=\"prod.php?pid=" . $prod->id . "\" class=\"product-detail\">Details</a>" .
+									"</div>" .
+									"<div class=\"col-sm-6\">" .
+										"<a href=\"\" class=\"product-action\" data-pid=\"" . $prod->id . "\" 
+									data-cat=\"" . $prod->cat_id . "\">Purchase</a>" .
+									"</div>" .		
+								"</div>" .
+							"</div>" .
+						"</div>";
+					}
+				?>
+		</div>
+		<div id="aside" class="col-sm-3">
+			<div class="row text-center">
+				<h3 class="col-sm-12">Categories</h3>
+			</div>
+			<div class="row text-center">
+				<ul class="col-sm-12">
+				<?php 
+					$stmt = $conn->prepare("SELECT * FROM category");
+					$stmt->execute();
+					$rs=$stmt->fetchall();
+					foreach($rs as $cat){
+						$n = strlen($cat->name) > 25 ? substr($cat->name, 0, 25) . ". . ." : $cat->name;
+						echo "<li><a href=\"products.php?cat=" . $cat->id . "\">" . $n ."</a></li>";
+					}
+				?>
+				</ul>
+			</div>
+		</div>
+	</div>

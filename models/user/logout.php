7b800a12dc4730da3505f9cb3a6e0a574e03f4ec
BZVZ <BZVZ@gmail.com>
2019-06-03 15:04:11 +0200

    Remove session after logout

--- models/user/logout.php
+++ models/user/logout.php
@@ -1,5 +1,9 @@
 <?php
 	session_start();
+
+	$stmt = $conn->prepare("DELETE FROM session WHERE uid = ? AND hash = ?");
+	$stmt->execute([$_SESSION["uid"], $_COOKIE["session-id"]]);
+
 	session_unset();
 	session_destroy();
 	setcookie("session-id", null, -1, SUBFOLDER . "/");
